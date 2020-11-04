4f0389fe96f08b493eb4f97428d5d7ba7656fff2
StamirskiK <01123929@pw.edu.pl>
2020-11-04 19:47:18 +0000

    Pierwsze zadanie zrobione

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+struct Wektor2D
+{
+    Wektor2D(){x=}
+    void   printxy() { std::cout << x << ';' << y << '\n'; }
+    double x, y;
+    double norm() { return sqrt(x * x + y * y); }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D v1;
+    v1.x = 5;
+    v1.y = 6;
+    v1.printxy();
+    cout << v1.norm();
 }
+
+/*
+int main()
+
+
+{struct Wektor2D{Wektor2D(){x = 4;
+y = 3;
+std::cout << "[" << x << "; " << y << "]" << '\n';
+}
+double x, y;
+void   norm()
+{
+    std::cout << sqrt(x * x + y * y) << '\n';
+}
+void print()
+{
+    std::cout << "[" << x << "; " << y << "]" << '\n';
+}
+}
+// void printxy() { std::cout << x << ';' std::cout << y << '\n'; } sqrt(x * x + y * y)
+}
+;
+
+Wektor2D v;
+v.x = 1;
+v.y = 3;
+v.norm();
+v.print();
+}
+*/
--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,19 @@
 class Wektor2D
 {
+   double X, Y;
+   
     // Tutaj napisz implementacje klasy Wektor2D
 };
+
+
+Wektor2D v1{};           // Konstruktor domyślny, wektor o wsp. [0, 0]
+v1.setX(1.);             // setter
+v1.setY(1.);             // setter
+double x1 = v1.getX(1.); // getter
+double y1 = v1.getY(1.); // getter
+ 
+Wektor2D v2{2., 2.}; // Konstruktor nadający współrzędne
+ 
+Wektor2D sum = v1 + v2; // dodawanie wektorów
+ 
+double prod = v1 * v2; // iloczyn skalarny
